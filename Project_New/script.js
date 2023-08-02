05710ce4bfb74fe1fe6850f37ce4d834f77da108
Ramyalakshmi19 <126234051+Ramyalakshmi19@users.noreply.github.com>
2023-08-03 04:44:47 +0530

    Add files via upload

---
+++ Project_New/script.js
@@ -0,0 +1,10 @@
+const sidebar=document.getElementById("Sidebar");
+const iconbar=document.getElementById("IconBar");
+const icons=document.querySelector(".sideicon");
+// iconbar.addEventListener('mouseover', () => {
+//   sidebar.style.display = 'block';
+// });
+
+// iconbar.addEventListener('mouseout', () => {
+//   sidebar.style.display = 'none';
+// });
